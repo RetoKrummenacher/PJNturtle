17159b25b984c7d2e85d8c3ef0249a1ba65d13c1
RetoKrummenacher <reto.krummenacher@stud.unibas.ch>
2022-02-28 23:56:55 +0100

    Update commands.py

--- pjnturtle/commands.py
+++ pjnturtle/commands.py
@@ -7,11 +7,11 @@ def draw_test():
     
     w, h = 1000,1000
     
-    img = Image.new('RGB',(w,h), Color.WHITE)
+    img = Image.new('RGB',(w,h), (255,255,255))
     draw = ImageDraw.Draw(img)
     
     # draw axis    
-    draw.line((0,h/2,w,h/2),fill=Color.BLACK)
-    draw.line((w/2,0,w/2,h),fill=Color.BLACK)
+    draw.line((0,h/2,w,h/2), (0,0,0))
+    draw.line((w/2,0,w/2,h), (0,0,0))
     
     return img
