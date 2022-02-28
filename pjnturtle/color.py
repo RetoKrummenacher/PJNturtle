8e5277ae95c2d968c02b6aaf792ded7f1f059c64
RetoKrummenacher <reto.krummenacher@stud.unibas.ch>
2022-02-28 23:32:43 +0100

    First Test

--- pjnturtle/color.py
+++ pjnturtle/color.py
@@ -2,9 +2,11 @@
 
 from enum import Enum
 
-class Color(Enum):
-    BLACK = (0,0,0)
-    WHITE = (255,255,255)
+class Color:
+    
+    def __init__(self):
+        self.BLACK = (0,0,0)
+        self.WHITE = (255,255,255)
     
     
 
