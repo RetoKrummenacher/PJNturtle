a6d2a97dab3cd67464e6bb6bda69652fd9b60311
RetoKrummenacher <reto.krummenacher@stud.unibas.ch>
2022-02-28 23:04:19 +0100

    First Test

--- pjnturtle/__init__.py
+++ pjnturtle/__init__.py
@@ -1,3 +1,4 @@
 # This is to import methods from the package
 
 from .color import Color
+from .commands import Commands
--- pjnturtle/commands.py
+++ pjnturtle/commands.py
@@ -3,15 +3,17 @@ from PIL import Image, ImageDraw
 
 from .color import Color
 
-def draw_test():
+class Commands():
     
-    w, h = 1000,1000
-    
-    img = Image.new('RGB',(w,h),Color.WHITE)
-    draw = ImageDraw.Draw(img)
-    
-    # draw axis    
-    draw.line((0,h/2,w,h/2),fill=Color.BLACK)
-    draw.line((w/2,0,w/2,h),fill=Color.BLACK)
-    
-    return img
+    def draw_test():
+        
+        w, h = 1000,1000
+        
+        img = Image.new('RGB',(w,h),Color.WHITE)
+        draw = ImageDraw.Draw(img)
+        
+        # draw axis    
+        draw.line((0,h/2,w,h/2),fill=Color.BLACK)
+        draw.line((w/2,0,w/2,h),fill=Color.BLACK)
+        
+        return img
