52b8cde200d52789694c1b50dc2d2a58d91675df
RetoKrummenacher <reto.krummenacher@stud.unibas.ch>
2022-03-15 01:43:22 +0100

    Setup for Jupyter

--- pjnturtle/__init__.py
+++ pjnturtle/__init__.py
@@ -1,3 +1,9 @@
 # This is to import methods from the package
 
 from pjnturtle.turtle import *
+
+from pjnturtle.common.point import *
+from pjnturtle.common.utils import *
+from pjnturtle.common.penstate import *
+from pjnturtle.common.color import *
+from pjnturtle.common.colordict import *
