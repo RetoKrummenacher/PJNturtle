ba0ffbadbcc9f6f67c6a4549941e95602ecd61a0
RetoKrummenacher <reto.krummenacher@stud.unibas.ch>
2022-03-15 02:03:21 +0100

    Setup for Jupyter

--- pjnturtle/common/__init__.py
+++ pjnturtle/common/__init__.py
@@ -1,9 +1,7 @@
 # This is to import methods from the package
 
-__all__ = ['point', 'utils', 'penstate', 'color', 'colordict']
-
-from .point import *
-from .color import *
-from .colordict import *
-from .penstate import *
-from .utils import *
+from .point import Point
+from .color import Color
+from .colordict import ColorDict
+from .penstate import PenState
+from .utils import Utils
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding = ' utf-8') as rm:
 
 setuptools.setup(
     name = 'pjnturtle',
-    version = '0.0.18',
+    version = '0.0.19',
     author = 'Reto Krummenacher',
     author_email = 'reto.krummenacher@unibas.ch',
     description = 'Simple python turtle for Jupyter Notebooks',
