724230ef8bb1b0d989c5f31a725cd3de2a19dad6
RetoKrummenacher <reto.krummenacher@stud.unibas.ch>
2022-03-15 22:19:07 +0100

    Update turtle.py

--- pjnturtle/turtle.py
+++ pjnturtle/turtle.py
@@ -19,112 +19,114 @@ from pjnturtle.common.colordict import ColorDict
 class Turtle:    
     
     def __init__(self):
-        self.CANVAS_SIZE_X = 1024
-        self.CANVAS_SIZE_Y = 1024
+        self.__canvas_SIZE_X = 1024
+        self.__canvas_SIZE_Y = 1024
         
-        self.colors = ColorDict()
+        self.__colors = ColorDict()
                         
-        self.pen_color = self.colors.colorDict.get('BLACK')
+        self.__pen_color = self.__colors.colorDict.get('BLACK')
         # penstate enumeration, default down, thus drawing
-        self.pen_state = PenState.PEN_DOWN
+        self.__pen_state = PenState.PEN_DOWN
         # pensize in pixel, DEFAULT = 1
-        self.pen_size = 2
+        self.__pen_size = 2
         
-        self.canvas_color = self.colors.colorDict.get('WHITE')
+        self.__canvas_color = self.__colors.colorDict.get('WHITE')
         
         # new CANVAS
         self.clear()
                 
         # orientation of the turtle at start in degrees
         # 0 upwards, 180 downwards
-        self.orientation = 0
+        self.__orientation = 0
         
         # home point
-        self.center = Point(self.CANVAS_SIZE_X//2, self.CANVAS_SIZE_Y//2)
+        self.__center = Point(self.__canvas_SIZE_X//2, self.__canvas_SIZE_Y//2)
         
         # current postion, start at the center of CANVAS
-        self.current_position = self.center
+        self.__curent_position = self.__center
         
     def turnRight(self, angle):
-        self.orientation += angle
-        self.orientation = Utils.normalizeOrientation(self.orientation)        
+        self.__orientation += angle
+        self.__orientation = Utils.normalizeOrientation(self.__orientation)        
     
     def turnLeft(self, angle):
-        self.orientation -= angle        
-        self.orientation = Utils.normalizeOrientation(self.orientation)
+        self.__orientation -= angle        
+        self.__orientation = Utils.normalizeOrientation(self.__orientation)
     
     def forward(self, length):        
-        self.move(length)
+        self.__move(length)
 
     def backward(self, length):        
-        self.move(-length)            
+        self.__move(-length)            
             
-    def move(self, length):
+    # private emthod
+    def __move(self, length):
         goal_position = Utils.goal_position(length, 
-                                         self.orientation, 
-                                         self.current_position)
+                                         self.__orientation, 
+                                         self.__curent_position)
         
-        if self.pen_state is PenState(1):
-            self.draw(goal_position)
-            self.current_position = goal_position
+        if self.__pen_state is PenState(1):
+            self.__draw(goal_position)
+            self.__curent_position = goal_position
         else:
-            self.current_position = goal_position       
-                
-    def draw(self, goal_position):
-        self.canvas.line(self.current_position.getPoint() + goal_position.getPoint(),
-                         fill = self.pen_color.get_rgb(),
-                         width = self.pen_size)  
+            self.__curent_position = goal_position       
+            
+    # private method
+    def __draw(self, goal_position):
+        self.__canvas.line(self.__curent_position.getPoint() + goal_position.getPoint(),
+                         fill = self.__pen_color.get_rgb(),
+                         width = self.__pen_size)  
                 
     def penUp(self):
-        self.pen_state = PenState.PEN_UP
+        self.__pen_state = PenState.PEN_UP
         
     def penDown(self):
-        self.pen_state = PenState.PEN_DOWN
+        self.__pen_state = PenState.PEN_DOWN
         
     def clear(self):
-        self.img = Image.new('RGB',(self.CANVAS_SIZE_X, self.CANVAS_SIZE_Y),
-                        self.canvas_color.get_rgb())
-        self.canvas = ImageDraw.Draw(self.img)
+        self.__img = Image.new('RGB',(self.__canvas_SIZE_X, self.__canvas_SIZE_Y),
+                        self.__canvas_color.get_rgb())
+        self.__canvas = ImageDraw.Draw(self.__img)
     
     def penColor(self, color):
         # as Python does not support function overloading, we do type check
         # alternative: using multipledispatch package
         if type(color) is str:
-            self.pen_color = self.colors.colorDict.get(color)
+            self.__pen_color = self.__colors.colorDict.get(color)
         else:
-            self.pen_color = color        
+            self.__pen_color = color        
 
     def backgroundColor(self, color):
         if type(color) is str:
-            self.canvas_color = self.colors.colorDict.get(color)
+            self.__canvas_color = self.__colors.colorDict.get(color)
         else:
-            self.canvas_color = color      
+            self.__canvas_color = color      
         
     def addColor(self, name : str, color : Color):
-        self.colors.addColor(name, color)
+        self.__colors.addColor(name, color)
         
     def color(self, r: int, g: int, b: int) -> Color:
         return Color(r, g, b)    
         
     def penSize(self, size):
-        self.pen_size = size
+        self.__pen_size = size
         
     def home(self):
-        self.current_position = self.center
-        self.orientation = 0
+        self.__curent_position = self.__center
+        self.__orientation = 0
         
     def reset(self):
-        self.pen_size = 1
-        self.pen_color = self.colors.colorDict.get('BLACK')
+        self.__pen_size = 1
+        self.__pen_color = self.__colors.colorDict.get('BLACK')
         
     def goto(self, x, y):
-        self.current_position = Point(x,y)
+        self.__curent_position = Point(x,y)
         
     def setOrientation(self, angle):
-        self.orientation = angle        
+        self.__orientation = angle        
         
     def display(self):
-        display(self.img)
+        display(self.__img)
         
     def drawTurtle(self):
         with resources.path('pjnturtle.resources', 'turtle.png') as path:
@@ -135,35 +137,35 @@ class Turtle:
                 h = h // 2            
                 im = im.resize((w, h), resample = Image.BILINEAR)
                 # rotation is counter clockwise, we need clockwise
-                im = im.rotate(-self.orientation,
+                im = im.rotate(-self.__orientation,
                                 resample = Image.BILINEAR,
                                 expand = 1)
           
         w, h = im.size
         # use box for position calculated from current position
         # upper left and lower right pixel coordinates
-        upper_left_x = self.current_position.getX() - w // 2
-        upper_left_y = self.current_position.getY() - h // 2
+        upper_left_x = self.__curent_position.getX() - w // 2
+        upper_left_y = self.__curent_position.getY() - h // 2
         
         box = (upper_left_x, upper_left_y,
                upper_left_x + w, upper_left_y + h)
                             
-        self.img.paste(im, box, im)
+        self.__img.paste(im, box, im)
         
     def write(self, text, fontSize):
         with resources.path('pjnturtle.resources','times-ro.ttf') as path:
             font = ImageFont.truetype(str(path), fontSize)
-        self.canvas.text(self.current_position.getPoint(), text,
-                         self.pen_color.get_rgb(), font = font)
+        self.__canvas.text(self.__curent_position.getPoint(), text,
+                         self.__pen_color.get_rgb(), font = font)
                 
     def fill(self):
         
-        screen_point = self.current_position
+        screen_point = self.__curent_position
                 
-        pixels = self.img.load()
+        pixels = self.__img.load()
         color_to_fill = pixels[screen_point.x, screen_point.y]
         
-        if (color_to_fill == self.pen_color.get_rgb()):
+        if (color_to_fill == self.__pen_color.get_rgb()):
             return
         
         # using python dictionary
@@ -174,12 +176,12 @@ class Turtle:
         while(bool(d)):
             key, position = d.popitem()
             
-            if ((position.x > 0 and position.x < self.CANVAS_SIZE_X - 1 and
-                 position.y > 0 and position.y < self.CANVAS_SIZE_Y - 1) and
+            if ((position.x > 0 and position.x < self.__canvas_SIZE_X - 1 and
+                 position.y > 0 and position.y < self.__canvas_SIZE_Y - 1) and
                 (pixels[position.x, position.y] == color_to_fill)):
                 
                 # set color of pixel to pen color
-                pixels[position.x, position.y] = self.pen_color.get_rgb()
+                pixels[position.x, position.y] = self.__pen_color.get_rgb()
                 
                 for e in [(0,-1),(0,1),(-1,0),(1,0)]:
                     i, j = e
